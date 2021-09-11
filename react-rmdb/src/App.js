655bf91199ffcc6304fe7c9c607a69b95d550843
Krist Veruari <kveruari@gmail.com>
2021-09-11 11:34:13 -0600

    hashrouter

--- react-rmdb/src/App.js
+++ react-rmdb/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //routing
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 //components
 import Header from './components/Header';
